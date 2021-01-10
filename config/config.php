0e5a4ffdc1f0cfe01598777944ad769bba4227e6
aju <ajuchacko91@gmail.com>
2021-01-10 16:12:03 +0530

    adds more config items

--- config/config.php
+++ config/config.php
@@ -5,5 +5,15 @@ return [
     /*
      * Database table name that will be used in migration
      */
-    'table' => 'users'
+    'table' => 'users',
+
+    /*
+     * Duration for which otp will be valid. Default is 10 minutes / 600 seconds
+     */
+    'ttl' => 600,
+
+    /*
+     * Default otp length/size
+     */
+    'size' => 4,
 ];
